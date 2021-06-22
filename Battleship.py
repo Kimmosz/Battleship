7f8cde80b7a58c91b0b2eb80d1df933cae68ceb1
Kimberly S <kimberlys99@icloud.com>
2021-06-22 19:06:12 +0200

    Handle gameplay
    Players are now able to shoot at each other and when one player has no boats left, the game stops.

--- Battleship.py
+++ Battleship.py
@@ -1,23 +1,34 @@
 # Function to clear the screen
 clear = lambda: print('\n' * 100)
 
+# Function that asks user to press enter to continue
+pressEnter = lambda: input('\nPress enter to continue ')
+
 # Class of the playerfields 
 class PlayerSea:
     def __init__(self, name):
         self.name = name
         self.privateField = [0 for index in range(419)]
         self.playField = [' ⛋ ' for index in range(419)]
+        self.score = 0
 
-    def setPrivateField(self, fieldIndexes):
+    # Function to set the private fields
+    def setAllPrivateFields(self, fieldIndexes):
         for index in fieldIndexes:
             self.privateField[index] = 1
 
+    # Function to set the current private field
+    def setPrivateField(self, fieldIndex):
+        self.privateField[fieldIndex] = 0
+
+    # Function to set the current play field
     def setPlayField(self, fieldIndex, isHit):
         if isHit: 
             self.playField[fieldIndex] = ' ⛝ '
         else:
             self.playField[fieldIndex] = ' ⛞ '
-    
+
+    # Function to show the current private or play field
     def showPlayField(self, isPlayField):
         fieldIndex = 0
         columnCounter = 0
@@ -58,6 +69,7 @@ class PlayerSea:
         boardOutput+=f'  [{rowIndexes}] \n'
         print(boardOutput)
 
+    # Function where player can give a position for the current ship
     def shipPlacementPosition(self, currentShip):
         placementPosition = 0
         self.showPlayField(False)
@@ -72,39 +84,46 @@ class PlayerSea:
                 continue
             
         
-
+    # Function where player can place the current ship horizontal or vertical
     def shipPlacementRotation(self, currentShip, placementPosition):
         placementRotation = ''
         while True: 
             placementRotation = input('Do you want to place your boat horizontal or vertical? (H/V) : ')
-            
+
+            # Place ship horizontal
             if placementRotation.upper() == 'H':
                 shipSize = placementPosition + (currentShip.size - 1)
                 if ((placementPosition >= 0 and placementPosition <= 19) and shipSize <= 19) or ((placementPosition >= 20 and placementPosition <= 39) and shipSize <= 39) or ((placementPosition >= 40 and placementPosition <= 59) and shipSize <= 59) or ((placementPosition >= 60 and placementPosition <= 79) and shipSize <= 79) or ((placementPosition >= 80 and placementPosition <= 99) and shipSize <= 99) or ((placementPosition >= 100 and placementPosition <= 119) and shipSize <= 119) or ((placementPosition >= 120 and placementPosition <= 139) and shipSize <= 139) or ((placementPosition >= 140 and placementPosition <= 149) and shipSize <= 149) or ((placementPosition >= 160 and placementPosition <= 179) and shipSize <= 179) or ((placementPosition >= 180 and placementPosition <= 199) and shipSize <= 199) or ((placementPosition >= 200 and placementPosition <= 219) and shipSize <= 219) or ((placementPosition >= 220 and placementPosition <= 239) and shipSize <= 239) or ((placementPosition >= 240 and placementPosition <= 259) and shipSize <= 259) or ((placementPosition >= 260 and placementPosition <= 279) and shipSize <= 279) or ((placementPosition >= 280 and placementPosition <= 299) and shipSize <= 299) or ((placementPosition >= 300 and placementPosition <= 319) and shipSize <= 319) or ((placementPosition >= 320 and placementPosition <= 339) and shipSize <= 339) or ((placementPosition >= 340 and placementPosition <= 359) and shipSize <= 359) or ((placementPosition >= 360 and placementPosition <= 379) and shipSize <= 379) or ((placementPosition >= 380 and placementPosition <= 399) and shipSize <= 399):
                     shipIndexes = []
                     position = placementPosition 
                     for size in range(currentShip.size):
-                        shipIndexes.append(position)
+                        shipIndexes.append(position)                    # Add to Player Sea
+                        currentShip.addCoordinate(position)             # Add coordinate to current Ship
                         position+=1
-                    self.setPrivateField(shipIndexes)
+                    self.setAllPrivateFields(shipIndexes)
+                    currentShip.getCoordinates()
                     break
                 else:
                     userInput = input('Not possible at this position, press enter to try again or (R) to choose another position : ')
+                    # Replace position
                     if userInput.upper() == 'R':
                         self.shipPlacementPosition(currentShip)
                         break
                     else:
                         continue
-                    
+
+            # Place ship vertical
             elif placementRotation.upper() == 'V':
                 shipSize = placementPosition + ((currentShip.size-1)*20)
                 if shipSize <= 399:
                     shipIndexes = []
                     position = placementPosition
                     for size in range(currentShip.size):
-                        shipIndexes.append(position)
+                        shipIndexes.append(position)                    # Add to Player Sea
+                        currentShip.addCoordinate(position)             # Add coordinate to current Ship
                         position+=20
-                    self.setPrivateField(shipIndexes)
+                    self.setAllPrivateFields(shipIndexes)
+                    currentShip.getCoordinates()
                     break
                 else:
                     userInput = input('Not possible at this position, press enter to try again or (R) to choose another position : ')
@@ -117,44 +136,62 @@ class PlayerSea:
             else:
                 print('Wrong input, try again')
                 continue
-    
+
+    # Function to check if shot is a hit or a miss
     def checkShot(self, fieldIndex):
-        if self.privateField[fieldIndex] == 1:
-            playField(fieldIndex, True)
+        if self.privateField[fieldIndex] != 0:
+            self.setPrivateField(fieldIndex)
+            self.setPlayField(fieldIndex, True)
             print('HIT!\n')
+            pressEnter()
             return True
         else:
-            playField(fieldIndex, False)
+            self.setPlayField(fieldIndex, False)
             print('MISS!\n')
+            pressEnter()
+            return False
+
+    # Function to check if there are ships left
+    def shipsLeft(self):
+        if 1 in self.privateField:
+            return True
+        else:
             return False
     
 # Head class of the ships
 class Ship:
-    def __init__(self, name, size):
+    def __init__(self, shipId, name, size):
+        self.shipId = shipId
         self.name = name
         self.size = size
         self.coordinates = []
 
     def getName():
         print(f'This ship is a {name}')
-        
-    def getCoordinates():
-        print(coordinates)
+
+    def addCoordinate(self, coordinate):
+        self.coordinates.append(coordinate)
 
 # Class of the Patrol Ship
 class Patrol(Ship):
-    def setCoordinates():
-        print('TBA')
+    def getCoordinates(self):
+        print(f'This Patrol Ship is at: {self.coordinates}')
+        pressEnter()
 
-# Class of the Cruiser Ship
+# Class of the Cruiser
 class Cruiser(Ship):
-    def setCoordinates():
-        print('TBA')
+    def getCoordinates(self):
+        print(f'This Cruiser is at: {self.coordinates}')
+        pressEnter()
 
 # Class of the Aircraft Carrier
 class AircraftCarrier(Ship):
-    def setCoordinates():
-        print('TBA')
+    def getCoordinates(self):
+        print(f'This Aircraft Carrier is at: {self.coordinates}')
+        pressEnter()
+
+# Function to create new set of ships
+createNewShips = lambda: [Patrol(1, 'Patrol Ship', 2), Patrol(2, 'Patrol Ship', 2), Cruiser(3, 'Cruiser', 3), Cruiser(4, 'Cruiser', 3), AircraftCarrier(5, 'Aircraft Carrier', 4), AircraftCarrier(6, 'Aircraft Carrier', 4)]
 
 # Function that manages the main menu
 def mainMenu():
@@ -175,9 +212,9 @@ def mainMenu():
 
 # Function that manages the local versus menu
 def localVersusMenu():
-    shipsPlayerOne = [Patrol('Patrol Ship', 2), Patrol('Patrol Ship', 2), Cruiser('Cruiser', 3), Cruiser('Cruiser', 3), AircraftCarrier('Aircraft Carrier', 4), AircraftCarrier('Aircraft Carrier', 4)]
-    shipsPlayerTwo = shipsPlayerOne #test
-
+    shipsPlayerOne = createNewShips()
+    shipsPlayerTwo = createNewShips()
+    
     clear()
     
     print('******** LOCAL VERSUS ********\n\n')
@@ -197,7 +234,7 @@ def localVersusMenu():
         placementIndex += 1
 
     playBoardOne.showPlayField(False)
-    input('Press enter to continue ')
+    pressEnter()
 
     clear()
     
@@ -209,17 +246,53 @@ def localVersusMenu():
         placementIndex += 1
 
     playBoardTwo.showPlayField(False)
-    input('Press enter to continue ')
+    pressEnter()
 
     # Start the game
-    #localVersusPlay([playBoardOne, playBoardTwo],[shipsPlayerOne, shipsPlayerTwo])
+    localVersusPlay([playBoardOne, playBoardTwo],[shipsPlayerOne, shipsPlayerTwo])
 
 # Function that manages the gameplay
 def localVersusPlay(boards, ships):
-    clear()
-    print('TBA')
+    playerTurn = 0
+    while True:
+        clear()
+        if playerTurn == 0:
+            print('******** Turn Player One ********')
+            boards[1].showPlayField(True)
+            fieldIndex = int(input('Give a number to shoot on [0-399] : '))
+            isHit = boards[1].checkShot(fieldIndex)
+            isFinished = boards[1].shipsLeft()
+            if isHit and not isFinished:
+                continue
+            elif isHit and isFinished:
+                print('***** Player One has won! *****')
+                boards[1].showPlayField(True)
+                pressEnter()
+                mainMenu()
+                break
+            else:
+                playerTurn = 1
+                continue
+                
+        else: 
+            print('******** Turn Player Two ********')
+            boards[0].showPlayField(True)
+            fieldIndex = int(input('Give a number to shoot on [0-399] : '))
+            isHit = boards[0].checkShot(fieldIndex)
+            shipsLeft = boards[0].shipsLeft()
 
-
+            if not shipsLeft:
+                print('\n***** Player Two has won! *****')
+                boards[0].showPlayField(True)
+                pressEnter()
+                mainMenu()
+                break
+            
+            if isHit:
+                continue   
+            else:
+                playerTurn = 0
+                continue
 
 
 mainMenu()
